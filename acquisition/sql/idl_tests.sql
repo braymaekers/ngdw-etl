07b218ae4a5726ed784b2cb757cbdf0ab14cd34f
p-jbarbosa <jbarbosa@pentaho.com>
2018-08-08 10:54:05 +0100

    updated idl test to have data_set and utilities.

--- acquisition/sql/idl_tests.sql
+++ acquisition/sql/idl_tests.sql
@@ -1,10 +1,10 @@
 
--- truncate table acquisition.role;
+-- truncate table acquisition.cspartm_stg;
 -- update acquisition.idl_file_control set control_row_count = null, row_count = null, status = null, status_description = null where status = 'success';
 
 CREATE TABLE acquisition.idl_file_control (
 	file_date int4 NULL,
-	filename varchar(64) NULL,
+	data_set varchar(64) NULL,
 	control_row_count int4 NULL,
 	row_count int4 NULL,
 	status varchar(8) NULL,
@@ -14,12 +14,12 @@ WITH (
 	OIDS=FALSE
 ) ;
 
-INSERT INTO acquisition.idl_file_control (file_date,filename,control_row_count,row_count,status,status_description) VALUES 
-(20160102,'role',NULL,NULL,NULL,NULL), (20160101,'role',NULL,NULL,NULL,NULL);
+INSERT INTO acquisition.idl_file_control (file_date,data_set,control_row_count,row_count,status,status_description) VALUES 
+(20160102,'CSPARTM',NULL,NULL,NULL,NULL), (20160101,'CSPARTM',NULL,NULL,NULL,NULL);
 
 
 CREATE TABLE acquisition.idl_file_metadata (
-	filename varchar(64) NULL,
+	data_set varchar(64) NULL,
 	field varchar NULL,
 	data_type varchar(32) NULL,
 	format varchar(32) NULL
@@ -28,9 +28,9 @@ WITH (
 	OIDS=FALSE
 ) ;
 
-INSERT INTO acquisition.idl_file_metadata (filename,field,data_type,format) VALUES 
-('role','id','integer',NULL), ('role','name','string',NULL), ('role','age','integer',NULL)
-,('role','role','string',NULL), ('role','date_role','date','yyyyMMdd');
+INSERT INTO acquisition.idl_file_metadata (data_set,field,data_type,format) VALUES 
+('CSPARTM','id','integer',NULL), ('CSPARTM','name','string',NULL), ('CSPARTM','age','integer',NULL)
+,('CSPARTM','CSPARTM','string',NULL), ('CSPARTM','date_role','date','yyyyMMdd');
 
 CREATE TABLE acquisition.idl_table_metadata (
 	target_table varchar(64) NULL,
@@ -42,9 +42,9 @@ WITH (
 
 
 INSERT INTO acquisition.idl_table_metadata (target_table,field) VALUES 
-('role','id'), ('role','name'), ('role','age'), ('role','role'), ('role','date_role');;
+('cspartm_stg','id'), ('cspartm_stg','name'), ('cspartm_stg','age'), ('cspartm_stg','role'), ('cspartm_stg','date_role');;
 
-CREATE TABLE acquisition."role" (
+CREATE TABLE acquisition."cspartm_stg" (
 	id int4 NULL,
 	"name" text NULL,
 	age int4 NULL,
